b482343852f55928880f29e417176c4c51f66197
Haroon Jawad <haroonjawad6@gmail.com>
2022-07-05 17:01:53 +0500

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,36 @@
+const Clarifai = require('clarifai');
+
+console.log(process.env.CLARIFAI_API_KEY )
+//You must add your own API key here from Clarifai. 
+const app = new Clarifai.App({
+ apiKey: process.env.CLARIFAI_API_KEY || '44'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
+    // entries[0] --> this used to return the entries
+    // TO
+    // entries[0].entries --> this now returns the entries
+    res.json(entries[0].entries);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+  handleImage,
+  handleApiCall
+}
